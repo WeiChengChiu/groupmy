bfc502325340e3d70dc4c5a76c025831f63291ce
WeiCheng <c3366301@gmail.com>
2018-08-14 16:37:09 +0800

    Implement group.posts CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,49 @@
 class PostsController < ApplicationController
+  before_action :find_group, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_post, only: [:edit, :update, :destroy]
+
+  def new
+    @post = @group.posts.build
+  end
+
+  def create
+    @post = @group.posts.new(post_params)
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+
+    redirect_to group_path(@group)
+  end
+
+  private
+
+  def find_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
