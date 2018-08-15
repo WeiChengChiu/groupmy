35d2b1717e14538f0e6b08b98bd5767b22e2dfe8
WeiCheng <c3366301@gmail.com>
2018-08-15 11:32:19 +0800

    Associate models: group, group_user, user

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,9 @@ class Group < ActiveRecord::Base
   has_many :posts
   belongs_to :owner, class_name: 'User', foreign_key: :user_id
 
+  has_many :group_users
+  has_many :members, through: :group_users, source: :user
+
   validates :title, presence: true
 
   def editable_by?(user)
