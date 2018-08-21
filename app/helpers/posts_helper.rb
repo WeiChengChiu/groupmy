f6e8ff04ecf9241f80400ddcb39e503b7413f5b0
WeiCheng <c3366301@gmail.com>
2018-08-21 15:31:15 +0800

    Refactor views by helper

---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,20 @@
+module PostsHelper
+  # format
+  def render_post_updated_at(post)
+    post.updated_at.to_s(:long)
+  end
+
+  # buttons
+  def rende_new_post_button_of_group(group)
+    if current_user
+      link_to("New Post", new_group_post_path(@group), class: "btn btn-mini btn-primary")
+    end
+  end
+
+  def render_post_management_button_of_group(group, post)
+    if current_user && post.editable_by?(current_user)
+      link_to("Edit", edit_group_post_path(post.group, post), class: "btn btn-mini") + " / " +
+      link_to("Delete", group_post_path(post.group, post), method: :delete, data: { confirm: "Are you sure?" })
+    end
+  end
+end
